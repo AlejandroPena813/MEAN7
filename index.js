0272e2b30543e87dce833c47eef8c0943818fbb0
Alejandro Pena <alejandropena17@gmail.com>
2019-04-23 13:26:51 -0400

    npm cors install. body-parset/cors and initial routing for web api.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+
 const mongoose = require('mongoose');
 const config  = require('./config/database'); //importing module.exports of DB
-
+// todo const authentication = require('./routes/authentication')(router); //passing it in this way is to share
+const bodyParser = require('body-parser');//acts as middleware to translate json
+const cors = require('cors');
 
 mongoose.Promise = global.Promise;//mongoose config
 mongoose.connect(config.uri, {useNewUrlParser: true}, (err) => {
@@ -11,6 +15,19 @@ mongoose.connect(config.uri, {useNewUrlParser: true}, (err) => {
     } else{
         console.log('Could connect to database: ', config.db);
     }
+});
+
+app.use(cors({ //once prd both running off same domain, so wont be needed.
+    origin: 'http://localhost:4200' //restrict access from origin url domain only
+}));
+app.use(bodyParser.urlencoded({ extended: false}));//middleware for json deserialization and into model. application/c-www-form-urlencoded accepted
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/client/dist/'));// TODO prod only
+// app.use('/api/authentication', authentication);
+
+app.get( '*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/dist/index/html')); //prod
 });
 
 app.get( '/', (req, res) => {
